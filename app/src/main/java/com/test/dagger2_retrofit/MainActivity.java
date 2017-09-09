cb39d0f713f9a8212a05767aa9e34442590f80b7
Pierre <pierrevanderpol@gmail.com>
2017-09-09 21:36:53 +0200

    comit dependencies

---
+++ app/src/main/java/com/test/dagger2_retrofit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.test.dagger2_retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
